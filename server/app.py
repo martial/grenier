677464f8a18dad5b714ecf3a89878d5c1b4689f1
Martial Geoffre-Rouland <martial@screen-club.com>
2023-04-27 16:46:16 +0200

    some upates – model + requirements

--- server/app.py
+++ server/app.py
@@ -207,7 +207,7 @@ def call_openai_gpt(prompt):
 
     # ChatGPT query
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",#"gpt-4",#
+        model="gpt-4-32k",#"gpt-4",#
         messages=conversation_history,
         max_tokens=2048,
         n=1,

f0a5c0c9c631f312b5b8a4e46ee533c6cede008b
Anthony Couret <hello@studioantho.com>
2023-05-15 17:24:49 +0200

    setup for 2 mic version

--- server/app.py
+++ server/app.py
@@ -4,18 +4,19 @@ import json
 from pythonosc import udp_client, dispatcher, osc_server
 import os
 import subprocess
-
 from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
 from dotenv import load_dotenv
 # from flask_scss import Scss
-# import pyttsx3
-import re
-#from vosk import Model, KaldiRecognizer, SetLogLevel
 import threading
 from database import *
 import webbrowser
+from chatGPT import chatGPT
 
 load_dotenv() 
+
+# Define openAPI key
+api_key = os.getenv("API_KEY")
+openai.api_key = api_key
 
 with open("server-config.json", "r") as file:
     server_config = json.load(file)
@@ -26,143 +27,6 @@ server_port = server_config["ports"]["server"]
 pde_port = server_config["ports"]["server_to_pde"]
 transcript_port_client = server_config["ports"]["server_to_transcript"]
 transcript_port_server = server_config["ports"]["transcript_to_server"]
-
-# Create an OSC client
-client = udp_client.SimpleUDPClient(ip_address, pde_port)
-transcription_client = udp_client.SimpleUDPClient(ip_address, transcript_port_client)
-
-osc_address = "/status/"
-full_reply_content = 'listening'
-osc_message = full_reply_content
-client.send_message(osc_address, osc_message)
-transcription_client.send_message(osc_address, osc_message)
-
-transcription = ""
-
-playing_mode = "play"
-
-# Create the OSC server dispatcher and register the handler function
-
-def speak(text, language='fr'):
-    subprocess.run(["say", "-v", f"{language}", text])
-
-def sendTranscriptionConfig():
-
-    trans_config = getDBTransConfig()
-    osc_address = "/config/"
-    transcription_client.send_message(osc_address, [
-        trans_config["transcription_silence"], 
-        trans_config["transcription_restart"],
-        trans_config["language"]
-    ])
-
-def handle_get_config_message(address, *args):
-    sendTranscriptionConfig()
-
-def handle_speech_message(address, *args):
-    
-    global transcription
-    global status
-
-    if ( playing_mode == "pause" ):
-        return
-
-    transcription = str(args[0])
-    started_on_processing = (bool(args[1]))
-
-    if (transcription == "Stop"):
-        endOpenai()
-    
-    elif (status == "waiting" and not started_on_processing and transcription != ''):
-
-        osc_message = transcription.encode('utf-8')
-        osc_address = "/prompt/"
-        client.send_message(osc_address, osc_message)
-
-def handle_stop_message(address, *args):
-
-    global status
-
-    if ( status == "processing"):
-        endOpenai()
-        osc_address = "/stopped/"
-        transcription_client.send_message(osc_address, [1])
-
-def handle_end_speech_message(address, *args):
-
-    global status
-    global transcription
-    global playing_mode
-
-    if ( playing_mode == "pause" ):
-        return
-    
-    started_on_processing = (bool(args[0]))
-
-    # Traitement du message OSC reçu
-    if (status == "waiting" and not started_on_processing and transcription != ''):
-        call_openai_gpt(transcription)
-    
-    transcription = ""
-
-    if ( status == "will_waiting"):
-        status = "waiting"
-        osc_address = "/status/"
-        osc_message = 'listening'
-        client.send_message(osc_address, osc_message)
-        transcription_client.send_message(osc_address, osc_message)
-
-def start_osc_server():
-
-    # Création du dispatcher qui gère les messages OSC reçus
-    dispatch = dispatcher.Dispatcher()
-    dispatch.map("/speech", handle_speech_message)    
-    dispatch.map("/end-speech", handle_end_speech_message)    
-    dispatch.map("/stop", handle_stop_message)    
-    dispatch.map("/get-config", handle_get_config_message)    
-
-    # Création du serveur OSC
-    server = osc_server.ThreadingOSCUDPServer((ip_address, transcript_port_server), dispatch)
-    print("OSC server started on {}:{}".format(ip_address, transcript_port_server))
-
-    # Démarrage du serveur OSC dans un thread dédié
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.start()
-
-def start_parameter_loop():
-
-    global playing_mode
-    global status
-
-    def run_job():
-
-        global playing_mode
-        global status
-
-        while True:
-            c_playing_mode = getDBPlayingMode()
-            if ( playing_mode == "play" and c_playing_mode == "pause" ):
-                status = "waiting"
-                transcription = ""
-                end_it = False
-                osc_address = "/status/"
-                osc_message = 'pause'
-                client.send_message(osc_address, osc_message)
-                transcription_client.send_message(osc_address, osc_message)
-                playing_mode = c_playing_mode
-            if ( playing_mode == "pause" and c_playing_mode == "play" ):
-                osc_address = "/status/"
-                osc_message = 'listening'
-                client.send_message(osc_address, osc_message)
-                transcription_client.send_message(osc_address, osc_message)
-                playing_mode = c_playing_mode
-
-
-    thread = threading.Thread(target=run_job)
-    thread.start()
-
-# Define openAPI key
-api_key = os.getenv("API_KEY")
 
 # Create database
 # createDB()
@@ -183,193 +47,189 @@ model = res["model"]
 
 resetDBModes()
 
-openai.api_key = api_key
-state = None
-conversation_history = [{"role": "system", "content": gpt_role}]
-conversation_history.append({"role": "system", "content": gpt_context})
-conversation_history.append({"role": "system", "content": gpt_action})
+gpt = chatGPT(ip_address,transcript_port_client)
+gpt.appendHistory({"role": "system", "content": gpt_role})
+gpt.appendHistory({"role": "system", "content": gpt_context})
+gpt.appendHistory({"role": "system", "content": gpt_action})
+
+# Create an OSC client
+client = udp_client.SimpleUDPClient(ip_address, pde_port)
+
+osc_address = "/status/"
+osc_message = 'listening'
+client.send_message(osc_address, osc_message)
+gpt.sendStatusMessage(osc_message)
+
+playing_mode = "play"
+
+# Create the OSC server dispatcher and register the handler function
+
+def speak(text, language='fr'):
+    subprocess.run(["say", "-v", f"{language}", text])
+
+def sendTranscriptionConfig():
+    trans_config = getDBTransConfig()
+    osc_address = "/config/"
+    gpt.sendConfigMessage([
+        trans_config["transcription_silence"], 
+        trans_config["transcription_restart"],
+        trans_config["language"]
+    ])
+
+def handle_get_config_message(address, *args):
+    sendTranscriptionConfig()
+
+def handle_speech_message(address, *args):
+    
+    global gpt
+
+    if ( playing_mode == "pause" ):
+        return
+
+    transcription = str(args[0])
+    gpt.setTranscription(transcription)
+    started_on_processing = (bool(args[1]))
+
+    if (transcription == "Stop"):
+        gpt.setEndIt(True)
+  
+    elif (gpt.getStatus() == "waiting" and not started_on_processing and transcription != ''):
+        osc_message = transcription.encode('utf-8')
+        osc_address = "/prompt/"
+        client.send_message(osc_address, osc_message)
+
+def handle_stop_message(address, *args):
+
+    global gpt
+    if ( gpt.getStatus() == "processing"):
+        gpt.setEndIt(True)
+        osc_address = "/stopped/"
+        gpt.sendStopMessage()
+
+def handle_end_speech_message(address, *args):
+
+    global gpt
+    global playing_mode
+
+    if ( playing_mode == "pause" ):
+        return
+    
+    started_on_processing = (bool(args[0]))
+    transcription = gpt.getTranscription()
+
+    # Traitement du message OSC reçu
+    if (gpt.getStatus() == "waiting" and not started_on_processing and transcription != ''):
+        call_openai_gpt(transcription)
+    
+    gpt.clearTranscription()
+
+    if ( gpt.getStatus() == "will_waiting"):
+        gpt.setStatus("waiting")
+        osc_address = "/status/"
+        osc_message = 'listening'
+        client.send_message(osc_address, osc_message)
+        gpt.sendStatusMessage(osc_message)
+
+def start_osc_server():
+
+    # Création du dispatcher qui gère les messages OSC reçus
+    dispatch = dispatcher.Dispatcher()
+    dispatch.map("/speech/", handle_speech_message)    
+    dispatch.map("/end-speech/", handle_end_speech_message)    
+    dispatch.map("/stop/", handle_stop_message)    
+    dispatch.map("/get-config/", handle_get_config_message)    
+
+    # Création du serveur OSC
+    server = osc_server.ThreadingOSCUDPServer((ip_address, transcript_port_server), dispatch)
+    print("OSC server started on {}:{}".format(ip_address, transcript_port_server))
+
+    # Démarrage du serveur OSC dans un thread dédié
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+
+def start_parameter_loop():
+
+    global gpt
+
+    global playing_mode
+    global gpt_role
+    global gpt_context
+    global gpt_action
+    global gpt_temp
+    global talk
+    global language
+    global model
+
+    def run_job():
+
+        global playing_mode
+
+        while True:
+
+            c_playing_mode = getDBPlayingMode()
+
+            if ( playing_mode == "play" and c_playing_mode == "pause" ):
+                gpt.setStatus("waiting")
+                gpt.clearTranscription()
+                gpt.setEndIt(False)
+                osc_address = "/status/"
+                osc_message = 'pause'
+                client.send_message(osc_address, osc_message)
+                gpt.sendStatusMessage(osc_message)
+                playing_mode = c_playing_mode
+                print("pause")
+
+            if ( playing_mode == "pause" and c_playing_mode == "play" ):
+                osc_address = "/status/"
+                osc_message = 'listening'
+                client.send_message(osc_address, osc_message)
+                gpt.sendStatusMessage(osc_message)
+                playing_mode = c_playing_mode
+
+            # Check if config or history should be updated (changes from html form)
+            res = getDBUpdate()
+            if (res):
+                gpt_role = res["gpt_role"]
+                gpt_context = res["gpt_context"]
+                gpt_action = res["gpt_action"]
+                gpt_temp = res["gpt_temp"]
+                gpt.appendHistory({"role": "system", "content": gpt_role})
+                gpt.appendHistory({"role": "system", "content": gpt_context})
+                gpt.appendHistory({"role": "system", "content": gpt_action})
+
+            #check if reset history
+            res = getDBReset()
+            if (res == 1):
+                gpt.resetHistory()
+                gpt.appendHistory({"role": "system", "content": gpt_role})
+                gpt.appendHistory({"role": "system", "content": gpt_context})
+                gpt.appendHistory({"role": "system", "content": gpt_action})
+                setDBReset(0)
+
+            res = getDBTransConfig()
+            if(res):
+                talk = res["talk"]
+                language = res["language"]
+                model = res["model"]
+
+
+    thread = threading.Thread(target=run_job)
+    thread.start()
+
 
 app = Flask(__name__)
 app.debug = True # needed to scss to compile
 # Scss(app, static_dir='static/css/', asset_dir='static/scss/')
 
-status = "waiting"
-end_it = False
-
-def endOpenai():
-    global end_it
-    end_it = True
-
 def call_openai_gpt(prompt):
 
-    global status
-    global end_it
-
-    global conversation_history
-    global gpt_role
-    global gpt_context
-    global gpt_action
-    global talk
-    global language
-    global model
-
-    global gpt_temp
-    global playing_mode
-
+    global gpt
     if ( playing_mode == "pause" ):
         return
-
-
-    # Returns if empty prompt
     if prompt == '' :
         return
 
-    # Retuns if alreay processing (call_opainai_gpt is threaded)
-    if status == "processing":
-        return
-    if status == "will_waiting":
-        return
-
-    # Check if config or history should be updated (changes from html form)
-    res = getDBUpdate()
-    if (res):
-        gpt_role = res["gpt_role"]
-        gpt_context = res["gpt_context"]
-        gpt_action = res["gpt_action"]
-        gpt_temp = res["gpt_temp"]
-        conversation_history.append({"role": "system", "content": gpt_role})
-        conversation_history.append({"role": "system", "content": gpt_context})
-        conversation_history.append({"role": "system", "content": gpt_action})
-
-    #check if reset history
-    reset = getDBReset()
-    if (reset == 1):
-        conversation_history = [{"role": "system", "content": gpt_role}]
-        conversation_history.append({"role": "system", "content": gpt_context})
-        conversation_history.append({"role": "system", "content": gpt_action})
-        setDBReset(0)
-
-    res = getDBTransConfig()
-    if(res):
-        talk = res["talk"]
-        language = res["language"]
-        model = res["model"]
-        
-    # We will go to processing mode now
-    status = "processing"
-
-    print("REQUETTE "+prompt)
-
-    # Send the prompt to Pde
-    osc_address = '/chat/'
-    full_reply_content = ''
-    osc_message = full_reply_content
-    client.send_message(osc_address, osc_message)
-
-    # Send the new status to Pde
-    osc_address = "/status/"
-    full_reply_content = 'requesting'
-    osc_message = full_reply_content
-    client.send_message(osc_address, osc_message)
-    full_reply_content = 'processing'
-    osc_message = full_reply_content
-    transcription_client.send_message(osc_address, osc_message)
-
-    # Append prompt to history
-    conversation_history.append({"role": "user", "content": prompt})
-
-    # ChatGPT query
-    try:
-        response = openai.ChatCompletion.create(
-            model=model,#gpt-4
-            messages=conversation_history,
-            max_tokens=2048,
-            n=1,
-            stop=None,
-            temperature=gpt_temp,
-            stream=True
-        )
-    except InvalidRequestError as e:
-        print(e)
-        osc_message = ("InvalidRequestError — Tokens exceeeded").encode('utf-8')
-        osc_address = "/chat/"
-        client.send_message(osc_address, osc_message)
-        status = "will_waiting"
-        end_it = False
-        conversation_history = []
-        conversation_history.append({"role": "system", "content": gpt_role})
-        conversation_history.append({"role": "system", "content": gpt_context})
-        conversation_history.append({"role": "system", "content": gpt_action})
-        call_openai_gpt(prompt)
-        return
-    except openai.errors.RateLimitError as e:
-        print(e)
-        osc_message = ("RateLimitError — Server is overloaded").encode('utf-8')
-        osc_address = "/chat/"
-        client.send_message(osc_address, osc_message)
-        status = "will_waiting"
-        end_it = False
-
-        return
-        
-    osc_address = "/status/"
-    full_reply_content = 'processing'
-    osc_message = full_reply_content
-    client.send_message(osc_address, osc_message)
-
-    # Process ChatGPT response, word to word
-    collected_chunks = []
-    collected_messages = []
-    last_spoken_phrase = ""
-
-    for chunk in response:
-
-        if ( playing_mode == "pause" ):
-            return
-
-        collected_chunks.append(chunk)
-        chunk_message = chunk['choices'][0]['delta']
-        collected_messages.append(chunk_message)
-
-        full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
-
-        # We stop the processing loop if we get a "stop" message
-        if end_it: 
-            break
-
-        
-        if(talk == 1):
-            phrases = re.split(r'[\.\?!]\s*', full_reply_content.strip())
-            if len(phrases) > 1 and phrases[-2] != last_spoken_phrase:
-                last_spoken_phrase = phrases[-2]
-                
-                voice = 'Allison' 
-                if language == 'fr':
-                    voice = 'Thomas'
-                speak(last_spoken_phrase, voice)  # Thomas is a French voice on macOS
-
-
-        if full_reply_content:
-
-            # Send the chatGPT response to Pde
-            osc_message = full_reply_content.encode('utf-8')
-            osc_address = "/chat/"
-            client.send_message(osc_address, osc_message)
-
-            #print(full_reply_content)
-    
-    # Append chatGPT response to history
-    full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
-    conversation_history.append({"role": "assistant", "content": full_reply_content})
-
-    status = "will_waiting"
-    end_it = False
-
-
-
-# Mapping function, map a value from input interval to output interval
-def map_range(value, from_min, from_max, to_min, to_max):
-    return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
+    gpt.callOpenAI(prompt, openai, gpt_role, gpt_context, gpt_action, model, gpt_temp, language, playing_mode, talk, True, client)
 
 
 @app.route('/config')
@@ -407,9 +267,6 @@ def config(model= None, talk = None, language=None, gpt_role=None, gpt_context=N
         playing_mode = getDBPlayingMode()
 
     return render_template('index.html', 
-        talk=talk,
-        model=model,
-        language=language,
         gpt_role=gpt_role, 
         gpt_context=gpt_context, 
         gpt_action=gpt_action, 
@@ -417,6 +274,9 @@ def config(model= None, talk = None, language=None, gpt_role=None, gpt_context=N
         updated=updated,
         transcription_silence=transcription_silence,
         transcription_restart=transcription_restart,
+        language=language,
+        talk=talk,
+        model=model,
         playing_mode=playing_mode
     )
 
@@ -443,12 +303,10 @@ def play():
     setDBPlayingMode("play")
     return redirect(url_for("config"))
 
-
 @app.route('/pause', methods=['POST'])
 def pause():
     setDBPlayingMode("pause")
     return redirect(url_for("config"))
-
 
 @app.route('/reset', methods=['POST'])
 def reset():
@@ -457,19 +315,14 @@ def reset():
 
 
 if __name__ == "__main__":
-        
-        
+         
     if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
         start_osc_server()
         start_parameter_loop()
 
-        # try:
-        # finally:
-        #     client._sock.close()
-        #     transcription_client._sock.close()
-
     sendTranscriptionConfig()
     webbrowser.open('http://'+ip_address+':'+str(server_port)+'/config')
+
     osc_address = "/status/"
     full_reply_content = 'listening'
     osc_message = full_reply_content
---
+++ server/chatGPT.py
@@ -0,0 +1,181 @@
+from pythonosc import udp_client
+from openai.error import InvalidRequestError
+import re
+import subprocess
+
+class chatGPT:
+
+    #transcription = ""
+
+    conversation_history = []
+    transcription = ""
+    end_it = False
+    status = "waiting"
+
+    def __init__(self, ip_address, transcript_port_client):
+        self.ip_address = ip_address
+        self.transcript_port_client = transcript_port_client
+        self.transcription_client = udp_client.SimpleUDPClient(ip_address, transcript_port_client)
+        osc_address = "/status/"
+        osc_message = 'listening'
+        self.transcription_client.send_message(osc_address, osc_message)
+
+    def resetHistory(self):
+        self.conversation_history = []
+
+    def appendHistory(self,data):
+        self.conversation_history.append(data)
+
+    def getHistory(self):
+        return self.conversation_history
+
+    def sendConfigMessage(self, data):
+        osc_address = "/config/"
+        self.transcription_client.send_message(osc_address,data)
+
+    def sendStopMessage(self):
+        osc_address = "/stopped/"
+        self.transcription_client.send_message(osc_address, [1])
+
+    def sendStatusMessage(self, osc_message):
+        osc_address = "/status/"
+        self.transcription_client.send_message(osc_address,osc_message)
+
+    def setTranscription(self, transcription):
+        self.transcription = transcription
+
+    def getTranscription(self):
+        return self.transcription
+    
+    def clearTranscription(self):
+        self.transcription = ""
+
+    def setStatus(self, status):
+        self.status = status
+
+    def getStatus(self):
+        return self.status
+
+    def setEndIt(self, end_it):
+        self.end_it = end_it
+
+    def getEndIt(self):
+        return self.end_it
+    
+    def speak(self, text, language='fr'):
+        subprocess.run(["say", "-v", f"{language}", text])
+    
+    def callOpenAI(self, prompt, openai, gpt_role, gpt_context, gpt_action, model, gpt_temp, language, playing_mode, talk, send_to_pde, pde_client):
+
+        if self.getStatus() == "processing":
+            return
+        if self.getStatus() == "will_waiting":
+            return
+        
+        self.setStatus("processing")
+    
+        print("REQUETTE "+prompt)
+
+        if (send_to_pde):
+            osc_address = '/chat/'
+            osc_message = ""
+            pde_client.send_message(osc_address, osc_message)    
+            osc_address = "/status/"
+            osc_message = 'requesting'
+            pde_client.send_message(osc_address, osc_message)
+        
+        osc_message = 'processing'
+        self.sendStatusMessage(osc_message)
+
+        self.appendHistory({"role": "user", "content": prompt})
+
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,#gpt-4
+                messages=self.getHistory(),
+                max_tokens=2048,
+                n=1,
+                stop=None,
+                temperature=gpt_temp,
+                stream=True
+            )
+
+        except InvalidRequestError as e:
+            print(e)
+            
+            if (send_to_pde):
+                osc_message = ("InvalidRequestError — Tokens exceeeded").encode('utf-8')
+                osc_address = "/chat/"
+                pde_client.send_message(osc_address, osc_message)
+            
+            self.setStatus("will_waiting")
+            self.setEndIt(False)
+            self.resetHistory()
+            self.appendHistory({"role": "system", "content": gpt_role})
+            self.appendHistory({"role": "system", "content": gpt_context})
+            self.appendHistory({"role": "system", "content": gpt_action})
+            self.callOpenAI(self, prompt, openai, gpt_role, gpt_context, gpt_action, model, gpt_temp, language, playing_mode, talk, send_to_pde, pde_client)
+        
+            return
+        
+        except openai.errors.RateLimitError as e:
+            print(e)
+
+            osc_message = ("RateLimitError — Server is overloaded").encode('utf-8')
+            osc_address = "/chat/"
+
+            if (send_to_pde):
+                pde_client.send_message(osc_address, osc_message)
+                self.setStatus("will_waiting")
+                self.setEndIt(False)
+
+            return
+
+        if (send_to_pde):
+            osc_address = "/status/"
+            osc_message = 'processing'
+            pde_client.send_message(osc_address, osc_message)
+
+        # Process ChatGPT response, word to word
+        collected_chunks = []
+        collected_messages = []
+        last_spoken_phrase = ""
+
+        for chunk in response:
+
+            if ( playing_mode == "pause" ):
+                return
+
+            collected_chunks.append(chunk)
+            chunk_message = chunk['choices'][0]['delta']
+            collected_messages.append(chunk_message)
+            full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+
+            # We stop the processing loop if we get a "stop" message
+            if self.getEndIt(): 
+                break
+
+            if(talk == 1):
+                phrases = re.split(r'[\.\?!]\s*', full_reply_content.strip())
+                if len(phrases) > 1 and phrases[-2] != last_spoken_phrase:
+                    last_spoken_phrase = phrases[-2]
+                    voice = 'Allison' 
+                    if language == 'fr':
+                        voice = 'Thomas'
+                    self.speak(last_spoken_phrase, voice)  # Thomas is a French voice on macOS
+
+
+            if full_reply_content and send_to_pde:
+                # Send the chatGPT response to Pde
+                osc_message = full_reply_content.encode('utf-8')
+                osc_address = "/chat/"
+                pde_client.send_message(osc_address, osc_message)
+
+                #print(full_reply_content)
+        
+        # Append chatGPT response to history
+        full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+        self.appendHistory({"role": "assistant", "content": full_reply_content})
+
+        self.setStatus("will_waiting")
+        self.setEndIt(False)
